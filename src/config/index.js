ed8af770c5b1c20d26b3026387a700bb7cf57476
Liu <1102549801@qq.com>
2024-04-18 02:30:16 +0800

    feat(其他设置): 网站基本设置，主题色
    - 网站基本设置，包括标题，logo大小等
    - 主题色有亮，色弱，暗黑

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+// 全局默认配置项
+// 首页地址（默认）
+export const HOME_URL = '/home/index';
+
+// 登录页地址（默认）
+export const LOGIN_URL = '/login';
+
+// pinia仓库前缀
+export const PINIA_PREFIX = 'nova-';
+
+// 默认主题颜色
+export const DEFAULT_THEME = '#2992FF';
+
+// 路由白名单地址（本地存在的路由 staticRouter.js 中）
+export const ROUTER_WHITE_LIST = ['/500'];
