df59613e6db37a296d8563023b50526d13bda932
Liu <1102549801@qq.com>
2024-04-18 03:01:08 +0800

    feat(路由): 安装路由，搭建静态路由和页面
    - 安装路由
    - 搭建静态路由和页面

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue';
 import './styles/index.scss'; // 引入样式
 import App from './App.vue';
+// 引入路由
+import router from './routers';
 // 引入仓库pinia
 import pinia from './stores/index.js';
 
 // 创建app
 const app = createApp(App);
+// 注册路由
+app.use(router);
 // 注册pinia
 app.use(pinia);
 // 挂载
