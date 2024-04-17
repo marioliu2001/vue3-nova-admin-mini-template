f8f41dc30b97eaeec01fd4529ea4ba5da6f0cd70
Liu <1102549801@qq.com>
2024-04-18 02:35:18 +0800

    feat(pinia): 安装注册
    - 安装
    - 注册

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue';
 import './styles/index.scss'; // 引入样式
 import App from './App.vue';
+// 引入仓库pinia
+import pinia from './stores/index.js';
 
 // 创建app
 const app = createApp(App);
+// 注册pinia
+app.use(pinia);
 // 挂载
 app.mount('#app');
