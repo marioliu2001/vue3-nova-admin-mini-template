cc375c93f18cc080383837024aa9fc45105d8576
Liu <1102549801@qq.com>
2024-04-19 02:06:53 +0800

    feat(Main主页面): Main主页面和放大缩小的叉叉
    - Main主页面
    - 右上角缩小页面的叉叉

---
+++ src/layouts/components/Main/components/Maximize.vue
@@ -0,0 +1,41 @@
+<script setup>
+// 退出全屏的叉叉按钮（右上角）
+import useGlobalStore from '@/stores/modules/global.js';
+
+const globalStore = useGlobalStore();
+
+const handleExitMaximize = () => {
+  globalStore.setGlobalState('maximize', false);
+};
+</script>
+
+<template>
+  <div class="maximize" @click="handleExitMaximize">
+    <el-icon :size="22" class="exitIcon"><CloseBold /></el-icon>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.maximize {
+  position: fixed;
+  top: -25px;
+  right: -25px;
+  z-index: 999;
+  width: 66px;
+  height: 66px;
+  cursor: pointer;
+  background-color: var(--el-color-primary-light-8);
+  border: 2px dashed var(--el-color-primary);
+  border-radius: 50%;
+  opacity: 0.9;
+  &:hover {
+    background-color: var(--el-color-primary-light-7);
+  }
+  .exitIcon {
+    position: relative;
+    top: 46%;
+    left: 19%;
+    color: var(--el-color-primary);
+  }
+}
+</style>
---
+++ src/layouts/components/Main/index.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref, watch, provide, onBeforeUnmount } from 'vue';
+import Maximize from './components/Maximize.vue';
+import { useDebounceFn } from '@vueuse/core';
+// import Tabs from "@/layouts/components/Tabs/index.vue"; 等下在做~
+import { storeToRefs } from 'pinia';
+import useKeepAliveStore from '@/stores/modules/keepAlive.js';
+import useGlobalStore from '@/stores/modules/global.js';
+
+const globalStore = useGlobalStore();
+// 路由动画
+const { transition } = storeToRefs(globalStore);
+
+const keepAliveStore = useKeepAliveStore();
+const { keepAliveName } = storeToRefs(keepAliveStore);
+
+// 刷新当前路由页面缓存方法
+const isRouterShow = ref(true);
+const refreshCurrentPage = (val) => (isRouterShow.value = val);
+provide('refresh', refreshCurrentPage);
+
+// 监听当前页面是否最大化，动态添加 class
+watch(
+  () => globalStore.maximize,
+  () => {
+    const app = document.getElementById('app');
+    if (globalStore.maximize) app.classList.add('main-maximize');
+    else app.classList.remove('main-maximize');
+    // 浏览器没有实际变化的情况下，触发一次浏览器尺寸变化的逻辑。保证全屏切换的时候，表格阔以进行自适应。
+    const event = new Event('resize');
+    window.dispatchEvent(event);
+  },
+  { deep: true, immediate: true }
+);
+
+/** 监听窗口大小变化，折叠侧边栏 */
+const screenWidth = ref(0);
+const showTabs = ref(true);
+const listeningWindow = useDebounceFn(() => {
+  screenWidth.value = document.body.clientWidth;
+  if (!globalStore.isCollapse && screenWidth.value < 1200)
+    globalStore.setGlobalState('isCollapse', true);
+  if (globalStore.isCollapse && screenWidth.value > 1200)
+    globalStore.setGlobalState('isCollapse', false);
+  if (screenWidth.value < 520) {
+    showTabs.value = false;
+  } else {
+    showTabs.value = true;
+  }
+}, 100);
+window.addEventListener('resize', listeningWindow, false);
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', listeningWindow);
+});
+</script>
+
+<template>
+  <Maximize v-show="globalStore.maximize" />
+  <!--<Tabs v-if="showTabs" /> 等下在做~ -->
+  <router-view v-slot="{ Component, route }">
+    <transition :name="transition" mode="out-in" appear>
+      <keep-alive :max="16" :include="keepAliveName">
+        <component
+          :is="Component"
+          :key="route.fullPath"
+          v-if="isRouterShow"
+          class="bg-[#F6F9FE] dark:bg-black"
+        />
+      </keep-alive>
+    </transition>
+  </router-view>
+</template>
+
+<style lang="scss" scoped>
+@import '../../../styles/transition.scss';
+</style>
