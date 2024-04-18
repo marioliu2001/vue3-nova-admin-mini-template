26c563dc0af1db7d34c96c155aeb5452a40d7089
Liu <1102549801@qq.com>
2024-04-18 22:48:25 +0800

    feat(收缩组件): 完成收缩组件
    - 完成收缩组件
    - 创建Header主组件

---
+++ src/layouts/components/Header/components/Collapse.vue
@@ -0,0 +1,20 @@
+<script setup>
+import useGlobalStore from '@/stores/modules/global.js';
+import { Fold, Expand } from '@element-plus/icons-vue';
+const globalStore = useGlobalStore();
+
+// 点击切换操作
+const changeCollapseIcon = () => {
+  // 定义图标切换变量(true-折叠，false-展开)
+  globalStore.isCollapse = globalStore.setCollapse(globalStore.isCollapse);
+};
+</script>
+
+<template>
+  <el-icon class="collapse-icon iq-icon" :size="20" @click="changeCollapseIcon">
+    <Fold v-if="!globalStore.isCollapse" />
+    <Expand v-else />
+  </el-icon>
+</template>
+
+<style scoped></style>
