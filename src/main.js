1e127b10b5068f500a921e44ce535a04209b5c9e
Liu <1102549801@qq.com>
2024-04-18 17:08:31 +0800

    feat(组件库暗黑主题): 组件库暗黑模式css
    - 引入组件库暗黑模式css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import './styles/index.scss'; // 引入样式
 import App from './App.vue';
+// 引入ElementPlus的暗黑模式css
+import 'element-plus/theme-chalk/dark/css-vars.css';
 // 引入路由
 import router from './routers';
 // 引入仓库pinia
