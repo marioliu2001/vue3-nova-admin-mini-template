b2c8f827a9f8d18afe27e9cf0fb4c4d97f48575f
Liu <1102549801@qq.com>
2024-04-19 15:57:05 +0800

    feat(App): 对App进行配置
    - 实现初始化主题配置
    - 实现Element-Plus尺寸配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,41 @@
-<script setup></script>
+<script setup>
+import { onMounted, computed } from 'vue';
+// import { useI18n } from 'vue-i18n';
+import { getBrowserLang } from '@/utils/index.js';
+import { useTheme } from '@/utils/theme.js';
+import en from 'element-plus/es/locale/lang/en';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+
+import useGlobalStore from '@/stores/modules/global.js';
+const globalStore = useGlobalStore();
+
+// 计算Element-Plus尺寸
+const dimension = computed(() => globalStore.dimension);
+const { initThemeConfig } = useTheme();
+
+// 初始化语言
+// const i18n = useI18n();
+onMounted(() => {
+  const language = globalStore.language ?? getBrowserLang();
+  // i18n.locale.value = language;
+  globalStore.setGlobalState('language', language);
+});
+
+// 语言配置
+const locale = computed(() => {
+  if (globalStore.language === 'zh') return zhCn;
+  if (globalStore.language === 'en') return en;
+  return getBrowserLang() === 'zh' ? zhCn : en;
+});
+
+// 初始化主题配置
+initThemeConfig();
+</script>
 
 <template>
-  <router-view />
+  <el-config-provider :locale="locale" :size="dimension">
+    <router-view />
+  </el-config-provider>
 </template>
 
 <style scoped></style>
